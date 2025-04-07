9dac98aab07cbd4a2ab47133fbbd0c2c806f8786
karen <karenquispego97@gmail.com>
2025-04-07 16:08:10 +0200

    fet

--- PrimerExcercici.R
+++ PrimerExcercici.R
@@ -1,3 +1,19 @@
 #Author: Karen Quispe
 #Date: 2/4/2025
-#Description: Primer Excercici
+#Description: excercici1
+
+#demanem els numeros a l'usuari
+num1 <- as.numeric(readline("Introdueix el primer numero: ")) 
+num2 <- as.numeric(readline("Introdueix el segon numero: "))
+
+# Fem les operacions
+suma <- num1 + num2
+resta <- num1 - num2
+producte <- num1 * num2   
+divisio <- num1 / num2
+
+# Mostrem els resultats
+cat("Suma: ", suma, "\n")
+cat("Resta: ", resta, "\n")
+cat("Producte: ", producte, "\n")
+cat("Divisio: ", divisio, "\n")
